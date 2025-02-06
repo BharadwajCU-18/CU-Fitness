a6c82a2b05c1a3d4b5efeef6facec2a9e104f2df
Bharadwaj Reddy <bangarubharadwajreddy18@gmail.com>
2025-02-06 12:49:36 -0500

    Straw Hats-7, Personal Information commit-1, contains UI of personal information page

--- personalInfo/views.py
+++ personalInfo/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+def personalInfo(request):
+    template = loader.get_template('personalInfo.html')
+    return HttpResponse(template.render())
+
+
+
