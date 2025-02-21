451782bc87671dbca0e7f5777bed705af192cf61
vinaykumarreddythangella <vinayreddyt19@gmail.com>
2025-02-20 23:17:19 -0500

    Fitness Information commit 4

--- personalInfo/models.py
+++ personalInfo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+# personalInfo/models.py
+from django.db import models
+
+class FitnessInformation(models.Model):
+    age = models.IntegerField()
+    height = models.FloatField()
+    weight = models.FloatField()
+    dietary_preferences = models.CharField(max_length=100)
+    fitness_goals = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Fitness Info: {self.age} years old"
