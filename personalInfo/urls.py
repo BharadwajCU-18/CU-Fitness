11771348bfbca4e05c49ee4c9fbd5ac260a54009
Saikumar <saikumarcheerneni013@gmail.com>
2025-03-15 17:08:00 -0400

    Resolved merge conflicts in urls.py, views.py, and db.sqlite3

--- personalInfo/urls.py
+++ personalInfo/urls.py
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
 # In your urls.py
 from django.urls import path
 from .views import home_view, register_view, fitnessInformation_view, login_view, dashboard_view, logout_view
-=======
 # from django.urls import path
 # from .views import home_view, register_view, login_view, dashboard_view, logout_view
-
 # urlpatterns = [
 #     path('', home_view, name='home'),
 #     path('register/', register_view, name='register'),
@@ -15,16 +12,13 @@ from .views import home_view, register_view, fitnessInformation_view, login_view
 # ]
 from django.urls import path
 from .views import home_view, register_view, fitnessInformation_view, login_view, dashboard_view, logout_view, chatbot_home, chatbot_response
->>>>>>> ui-ux-chatbot
+
 
 urlpatterns = [
     path('', home_view, name='home'),
     path('register/', register_view, name='register'),
-<<<<<<< HEAD
     path('fitnessInformation/', fitnessInformation_view, name='fitnessInformation'),  # Update to camel case
-=======
     path('fitnessinformation/', fitnessInformation_view, name='fitnessinformation'),  
->>>>>>> ui-ux-chatbot
     path('login/', login_view, name='login'),
     path('dashboard/', dashboard_view, name='dashboard'),
     path('logout/', logout_view, name='logout'),
