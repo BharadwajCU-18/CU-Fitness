8f1fef0a165fdd32782d39fa46636bb7b3ad5f3d
vinaykumarreddythangella <vinayreddyt19@gmail.com>
2025-03-22 17:40:03 -0400

    commit related to fitnessprogress 1

--- fitness/forms.py
+++ fitness/forms.py
@@ -1,15 +1,7 @@
-# from django import forms
-# from .models import FitnessReport
-
-# class FitnessForm(forms.ModelForm):
-#     class Meta:
-#         model = FitnessReport
-#         fields = ['pull_ups', 'cardio_time', 'cool_down_time', 'hours_of_sleep']
-
 from django import forms
-from .models import FitnessReport  # Ensure the model is imported correctly
+from .models import FitnessReport  
 
 class FitnessForm(forms.ModelForm):
     class Meta:
-        model = FitnessReport  # Make sure this is your actual model
+        model = FitnessReport  
         fields = ['pull_ups', 'cardio_time', 'cool_down_time', 'hours_of_sleep']
--- fitness/models.py
+++ fitness/models.py
@@ -10,11 +10,11 @@ class FitnessReport(models.Model):
         return f"Fitness Report - {self.id}"
 
     def calculate_calories_burned(self):
-        # Assuming an average calorie burn estimation for each activity:
-        calories_from_pullups = self.pull_ups * 0.25  # 0.25 calories per pull-up
-        calories_from_cardio = self.cardio_time * 8  # 8 calories per minute for cardio
-        calories_from_cool_down = self.cool_down_time * 2  # 2 calories per minute for cool down
-        calories_from_sleep = self.hours_of_sleep * 50  # 50 calories per hour of sleep (estimate)
+        
+        calories_from_pullups = self.pull_ups * 0.25  
+        calories_from_cardio = self.cardio_time * 8
+        calories_from_cool_down = self.cool_down_time * 2  
+        calories_from_sleep = self.hours_of_sleep * 50
 
         total_calories = calories_from_pullups + calories_from_cardio + calories_from_cool_down + calories_from_sleep
         return total_calories
