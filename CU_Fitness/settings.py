4ae73dc2e651848caef36d97ccee0f95a1590427
Saikumar <saikumarcheerneni013@gmail.com>
2025-03-25 18:22:32 -0400

    Chabot-HomePage

--- CU_Fitness/settings.py
+++ CU_Fitness/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kwwhj2_)&w%m!jc6njjvk9_@l4k7t1ly8qer9)4p0e^)2153qf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
